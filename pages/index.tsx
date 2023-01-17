ee06cdfb13cef6625efbe6e83405567b32960c88
Jan Kaifer <jan@kaifer.cz>
2023-01-17 15:34:20 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,15 @@
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default function Home({ test }) {
+  return <p>{JSON.stringify(test)}</p>;
+}
+
+export async function getServerSideProps({ resolvedUrl }) {
+  const name = "test";
+  console.log(resolvedUrl);
+  const test = require(`../${name}.json`);
+  return {
+    props: {
+      test,
+    },
+  };
 }
